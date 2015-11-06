5d7bb43c92509d4cb4a28ec68a33fefe6ea29a2e
Christoph Frick <rid@ofnir.net>
2015-11-06 17:02:55 +0100

    file component service does not need to hold on to the injected strategy list

--- src/main/groovy/vdecl/FileToComponentService.groovy
+++ src/main/groovy/vdecl/FileToComponentService.groovy
@@ -10,16 +10,14 @@ import java.util.regex.Pattern
 @Slf4j
 class FileToComponentService {
 
-    private final List<IFileToComponentStrategy> fileToComponentStrategies
-    private final Map<String,IFileToComponentStrategy> suffixToStrategy
     final Map<String,String> legend
+    private final Map<String,IFileToComponentStrategy> suffixToStrategy
     private final Pattern suffixPattern
 
     @Autowired
     FileToComponentService(List<IFileToComponentStrategy> fileToComponentStrategies) {
-        this.fileToComponentStrategies = fileToComponentStrategies
-        suffixToStrategy = fileToComponentStrategies.collectEntries{[it.suffix, it]}
         legend = fileToComponentStrategies.collectEntries{[it.suffix,it.description]}
+        suffixToStrategy = fileToComponentStrategies.collectEntries{[it.suffix, it]}
         suffixPattern = ~/.*\.(${suffixToStrategy.keySet().collect{Pattern.quote(it)}.join("|")})/
         log.debug "Found strategies: ${suffixToStrategy}"
     }
