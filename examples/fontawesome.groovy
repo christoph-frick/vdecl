ef3ae60a43b3475bd43fbbfce5c7afb537ebae39
Christoph Frick <rid@ofnir.net>
2015-11-29 16:00:06 +0100

    make the fontawesome example scrollable

--- examples/fontawesome.groovy
+++ examples/fontawesome.groovy
@@ -1,15 +1,20 @@
 import com.vaadin.ui.*
 import com.vaadin.server.FontAwesome
 
-new GridLayout().with {
-	columns = 4
-	FontAwesome.values().each{ fontIcon ->
-		addComponent(new Label().with{
-			caption = "${fontIcon.name()} (0x${Integer.toString(fontIcon.codepoint, 16)})"
-			icon = fontIcon
-			it
-		})
+new Panel(
+	new GridLayout().with {
+		columns = 4
+		FontAwesome.values().each{ fontIcon ->
+			addComponent(new Label().with{
+				caption = "${fontIcon.name()} (0x${Integer.toString(fontIcon.codepoint, 16)})"
+				icon = fontIcon
+				it
+			})
+		}
+		it
 	}
+).with{
+	setSizeFull()
+	addStyleName("borderless")
 	it
 }
-
