d9ef26ae6a7fe9bcd1bbec439821cffd637482ec
Christoph Frick <cf@ofnir.net>
2019-07-19 08:09:05 +0200

    Update examples

--- examples/login.clj
+++ examples/login.clj
@@ -1,10 +1,12 @@
-(let [width "40%"] 
+(let [width "40%"
+      show-error? false
+      error-message "Username unknown or wrong password"] 
   [:v-vertical-layout {:size-full true :margin true}
    [:v-label {:style-name "h1 colored" ":center" true ":expand" "0.2" :size-auto true} "Corp Inc" [:strong "App"]]
    [:v-panel {:caption "Please Login To Continue" ":expand" "0.8" ":center" true :width width}
     [:v-vertical-layout {:size-full true :margin true :spacing true}
-     [:v-label {:_id "error" :style-name "failure" :visible false} "Error message here"]
-     [:v-text-field {:_id "username" :size-full true :input-prompt "user@example.com" :caption "Username"}]
+     [:v-label {:_id "error" :style-name "failure" :visible show-error? :size-full true} error-message]
+     [:v-text-field {:_id "username" :size-full true :placeHolder "user@example.com" :caption "Username" :focus true}]
      [:v-password-field {:_id "password" :size-full true :caption "Password"}]
      [:v-horizontal-layout {:spacing true :width "100%"}
       [:v-button "Cancel"]
